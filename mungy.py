3b4b5b01d44cc63b607aaaa1db00e1949496335b
christobot <dxdstudio@gmail.com>
2014-08-03 21:45:01 -0700

    Add more robustness to library, fix some weird errors for wikipedia style html formatting.

--- mungy.py
+++ mungy.py
@@ -10,33 +10,55 @@ class MalformedHTMLError(Exception):
 class Munger():
     """Designed to be fluent/chainable."""
 
-    def json(self, data):
+    def to_json(self, data):
         return json.dumps(data)
+
+    def html_to_lists(self, pqhtml):
+        """Takes a chunk of html and generates multiple lists
+        from all found UL's on the page.
+        """
+        # TODO: Add OL support
+        list_html = []
+        for k, ul in enumerate(pq(pqhtml).find('ul')):
+            list_html.append(self.list_to_list(pq(ul)))
+        return list_html
 
     def dict_to_list(self, data):
         ls = []
-        for k, v in data.iteritems():
+        for _, v in data.iteritems():
             ls.append(v)
         return ls
 
     def list_to_list(self, list_html):
-        data = []
-        # ul > li to list of lists
+        """Takes a single ul node and converts it to a
+        list of list-items"""
+        list_items = []
         for li in pq(list_html).find('li'):
-            data.append(pq(li).text())
-        return data
+            list_items.append(pq(li).text())
+        return list_items
 
     def table_to_dict_values(self, table_html):
         # Sort by values
         data = {}
         headings = []
-        for k, heading in enumerate(pq(table_html).find('thead th')):
-            headings.append(pq(heading).text().strip().lower())
-        for k, row in enumerate(pq(table_html).find('tbody tr')):
-            data[k] = {}
-            for k1, cell in enumerate(pq(row).find('td')):
-                title = headings[k1]
-                data[k][title] = pq(cell).text().strip().lower()
+        # Try proper theader formatting first.
+        ths = pq(table_html).find('thead th')
+        if not ths:
+            ths = pq(table_html).find('th')
+        if not ths:
+            raise MalformedHTMLError('Invalid html. No THs could be found.')
+        for th in ths:
+            headings.append(pq(th).text().strip().lower())
+        trs = pq(table_html).find('tbody tr')
+        if not trs:
+            trs = pq(table_html).find('tr')
+        if not trs:
+            raise MalformedHTMLError('Invalid html. No TRs could be found.')
+        for k, tr in enumerate(trs):
+            tr_data = {}
+            for k1, td in enumerate(pq(tr).children()):
+                tr_data[headings[k1]] = pq(td).text().strip().lower()
+            data[k] = tr_data
         return data
 
     def table_to_dict_indices(self, table_html):
@@ -50,7 +72,6 @@ class Munger():
         }
         """
         # TODO: handle images
-
         # Handle plain tables, no headings
         if not pq(table_html).find('tbody') and not \
                 pq(table_html).find('thead'):
@@ -60,9 +81,9 @@ class Munger():
                             data[k].append(pq(cell).text().strip().lower())
         else:
             # Handle traditionally (properly) formatted tables
-            for k, heading in enumerate(pq(table_html).find('thead th')):
+            for _, heading in enumerate(pq(table_html).find('thead th')):
                 data[k] = (pq(heading).text().strip().lower(), [])
-            for k, row in enumerate(pq(table_html).find('tbody tr')):
+            for _, row in enumerate(pq(table_html).find('tbody tr')):
                 for k1, cell in enumerate(pq(row).find('td')):
                     try:
                         # Nested tables, etc will just
