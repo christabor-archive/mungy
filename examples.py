f72ff67be9cef3c2fe3856ee1d93dc82e4aa6750
christobot <dxdstudio@gmail.com>
2014-08-03 21:45:38 -0700

    Update method rename

--- examples.py
+++ examples.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     html = pq(filename='{}/comps.html'.format(os.getcwd()))
     f = mung.table_to_dict_values(html.find('#top10'))
     # f = mung.table_to_dict_indices(html.find('#top10'))
-    print mung.json(mung.dict_to_list(f))
+    print mung.to_json(mung.dict_to_list(f))
     print f
 
     # Single list
@@ -24,11 +24,11 @@ if __name__ == '__main__':
         key = pq(headlines).eq(k).text().strip().lower()
         if key:
             swords[key] = mung.list_to_list(pq(ul))
-    print mung.json(swords)
+    print mung.to_json(swords)
 
     # Multiple lists generic
     swords = {}
     listhtml = pq(filename='{}/syseng.html'.format(os.getcwd()))
     for k, ul in enumerate(pq(listhtml).find('ul')):
         swords[k] = mung.list_to_list(pq(ul))
-    print mung.json(swords)
+    print mung.to_json(swords)
